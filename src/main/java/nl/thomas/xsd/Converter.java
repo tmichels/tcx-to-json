5c0e462eb16be9d3544f06d505fb256232b94609
Thomas Michels <thomas_michels@outlook.com>
2023-10-20 11:54:39 +0200

    Implementeren uitlezen bestand

--- src/main/java/nl/thomas/xsd/Converter.java
+++ src/main/java/nl/thomas/xsd/Converter.java
@@ -1,11 +1,36 @@
 package nl.thomas.xsd;
 
-import java.util.List;
+import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import org.springframework.stereotype.Component;
 
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@Component
 public class Converter {
 
-    public void convert(List<String> fileContent) {
+    public TrainingCenterDatabaseT convert(String tcxContent) throws JAXBException {
+        Unmarshaller unmarshaller = createUnmarshaller();
+        Source source = createSourceFromString(tcxContent);
 
+        JAXBElement<TrainingCenterDatabaseT> root = unmarshaller.unmarshal(source, TrainingCenterDatabaseT.class);
+        return root.getValue();
+    }
+
+    private Unmarshaller createUnmarshaller() throws JAXBException {
+        return JAXBContext.newInstance(TrainingCenterDatabaseT.class).createUnmarshaller();
+    }
+
+    private Source createSourceFromString(String tcxContent) {
+        InputStream stream = new ByteArrayInputStream(tcxContent.getBytes(StandardCharsets.UTF_8));
+        return new StreamSource(stream);
     }
 
 }
--- src/main/java/nl/thomas/xsd/TcxController.java
+++ src/main/java/nl/thomas/xsd/TcxController.java
@@ -1,6 +1,11 @@
 package nl.thomas.xsd;
 
-import org.springframework.web.bind.annotation.*;
+import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
+import jakarta.xml.bind.JAXBException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,11 +13,24 @@ import java.nio.file.Path;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/convert")
 public class TcxController {
 
-    @PostMapping("/get-file")
-    public List<String> get(@RequestBody String location) throws IOException {
-        List<String> strings = Files.readAllLines(Path.of(location));
-        return strings;
+    private final Converter converter;
+
+    public TcxController(Converter converter) {
+        this.converter = converter;
+    }
+
+    @PostMapping("file")
+    public TrainingCenterDatabaseT file(@RequestBody String location) throws IOException, JAXBException {
+        List<String> lines = Files.readAllLines(Path.of(location));
+        String collect = String.join("\n", lines);
+        return converter.convert(collect);
+    }
+
+    @PostMapping("file-content")
+    public TrainingCenterDatabaseT getFromFileContent(@RequestBody String fileContent) throws JAXBException {
+        return converter.convert(fileContent);
     }
 }
