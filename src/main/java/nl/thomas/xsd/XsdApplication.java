6e4e4f7ca8e3544a513d5be7f4840a1e829cc010
Thomas Michels <thomas_michels@outlook.com>
2023-10-20 08:35:53 +0200

    Eerste commit

---
+++ src/main/java/nl/thomas/xsd/XsdApplication.java
@@ -0,0 +1,13 @@
+package nl.thomas.xsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsdApplication.class, args);
+	}
+
+}
