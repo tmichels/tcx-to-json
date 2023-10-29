cd993969c88018dc81611a52a3a25e0e1b151602
Thomas Michels <thomas_michels@outlook.com>
2023-10-29 10:08:03 +0100

    Add test convenience code (read file on startup)

--- src/main/java/nl/thomas/xsd/XsdApplication.java
+++ src/main/java/nl/thomas/xsd/XsdApplication.java
@@ -1,13 +1,35 @@
 package nl.thomas.xsd;
 
-import org.springframework.boot.SpringApplication;
+import jakarta.xml.bind.JAXBException;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class XsdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XsdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(XsdApplication.class)
+                .run(args);
+    }
+
+    @Component
+    static class DevCommandLineRunner implements CommandLineRunner {
+
+        private final TcxController tcxController;
+
+        DevCommandLineRunner(TcxController tcxController) {
+            this.tcxController = tcxController;
+        }
+
+
+        @Override
+        public void run(String... args) throws JAXBException, IOException {
+            tcxController.file("src/test/java/testfiles/valid.tcx");
+        }
+    }
 
 }
