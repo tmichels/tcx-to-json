e628344c2429f43668caf31433b2fccd260bc8ff
Thomas Michels <thomas_michels@outlook.com>
2023-10-31 08:07:50 +0100

    Improve ExtensionHandler, use XSD extension definition

--- src/main/java/nl/thomas/xsd/TrackpointExtensionHandler.java
+++ src/main/java/nl/thomas/xsd/TrackpointExtensionHandler.java
@@ -1,14 +1,24 @@
 package nl.thomas.xsd;
 
+import com.garmin.xmlschemas.activityextension.v2.ActivityTrackpointExtensionT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrackpointT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
 
-import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -17,77 +27,45 @@ public class TrackpointExtensionHandler {
 
     void setTrackpointSpeedFromExtension(TrainingCenterDatabaseT trainingCenterDatabaseT) {
         List<TrackpointT> trackpoints = TrainingCenterDatabaseExtractor.extractTrackpoints(trainingCenterDatabaseT);
-        trackpoints.forEach(trackpointT -> trackpointT.setSpeed(getSpeedFromExtensions(trackpointT)));
+        trackpoints.forEach(this::replaceIncorrectlyParsedExtension);
     }
 
-    private Double getSpeedFromExtensions(TrackpointT trackpointT) {
-        XMLGregorianCalendar trackpointTime = trackpointT.getTime();
-        if (trackpointT.getExtensions() == null || trackpointT.getExtensions().getAny() == null || trackpointT.getExtensions().getAny().isEmpty()) {
-            log.warn("No extensions found in trackpoint {}", trackpointTime);
-            return null;
-        }
-        List<Object> extensions = trackpointT.getExtensions().getAny();
-        for (Object extension : extensions) {
-            Double speedFromExtension = getSpeedFromExtension((Element) extension, trackpointTime);
-            if (speedFromExtension != null) {
-                return speedFromExtension;
-            }
-        }
-        return null;
+    private void replaceIncorrectlyParsedExtension(TrackpointT trackpointT) {
+        trackpointT
+                .getExtensions()
+                .getAny()
+                .set(0, parseExtension(trackpointT));
     }
 
-    private Double getSpeedFromExtension(Element element, XMLGregorianCalendar trackpointTime) {
-        if ("TPX".equals(element.getLocalName())) {
-            return getSpeedFromTPX(element, trackpointTime);
-        } else {
-            warnForUnexpectedExtensionNodes(element, trackpointTime);
+    private ActivityTrackpointExtensionT parseExtension(TrackpointT trackpointT) {
+        String rawXml = getRawXmlStringForExtension(trackpointT);
+        try {
+            return convertXmlToObject(rawXml);
+        } catch (JAXBException e) {
+            log.error("The TrackpointExtension is invalid and cannot be processed: \"{}\"", rawXml, e);
             return null;
         }
     }
 
-    private Double getSpeedFromTPX(Element element, XMLGregorianCalendar trackpointTime) {
-        NodeList childNodes = element.getChildNodes();
-        int childNodesLength = childNodes.getLength();
-        warnForUnexpectedNumberOfTpxNodes(childNodesLength, trackpointTime);
-        for (int i = 0; i < childNodesLength; i++) {
-            Double speed = getSpeedFromNode(trackpointTime, childNodes, i);
-            if (speed != null) {
-                return speed;
-            }
-        }
-        return null;
+    private String getRawXmlStringForExtension(TrackpointT trackpointT) {
+        Element trackpointExtension = (Element) trackpointT.getExtensions().getAny().getFirst();
+        Document document = trackpointExtension.getOwnerDocument();
+        DOMImplementationLS domImplLS = (DOMImplementationLS) document
+                .getImplementation();
+        LSSerializer serializer = domImplLS.createLSSerializer();
+        return serializer.writeToString(trackpointExtension);
     }
 
-    private Double getSpeedFromNode(XMLGregorianCalendar trackpointTime, NodeList childNodes, int i) {
-        Node node = childNodes.item(i);
-        if (node.getLocalName().equals("Speed")) {
-            return Double.parseDouble(node.getTextContent());
-        } else {
-            warnForUnexpectedTpxNodeTypes(node, trackpointTime);
-            return null;
-        }
-    }
+    private ActivityTrackpointExtensionT convertXmlToObject(String rawXml) throws JAXBException {
+        String correctedXml = rawXml.replace(
+                "Speed xmlns=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2\"",
+                "Speed xmlns=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\"");
+        correctedXml = correctedXml.replace("<?xml version=\"1.0\" encoding=\"UTF-16\"?>", "");
+        Unmarshaller unmarshaller = JAXBContext.newInstance(ActivityTrackpointExtensionT.class).createUnmarshaller();
+        InputStream stream = new ByteArrayInputStream(correctedXml.getBytes(StandardCharsets.UTF_8));
+        Source source = new StreamSource(stream);
 
-    private void warnForUnexpectedNumberOfTpxNodes(int childNodesLength, XMLGregorianCalendar trackpointTime) {
-        if (childNodesLength != 1) {
-            log.warn(
-                    "TPX node for Trackpoint {} did not have the expected amount of 1 children, but {} children",
-                    trackpointTime,
-                    childNodesLength);
-        }
-    }
-
-    private void warnForUnexpectedTpxNodeTypes(Node node, XMLGregorianCalendar trackpointTime) {
-        log.warn(
-                "Instead of speed node, the node {} with value {} was found for trackpoint {}",
-                node.getNodeName(),
-                node.getTextContent(),
-                trackpointTime);
-    }
-
-    private void warnForUnexpectedExtensionNodes(Element element, XMLGregorianCalendar trackpointTime) {
-        log.warn("Trackpoint {} had another other extension \"{}\" than the expected \"x:TPX\" extension",
-                trackpointTime,
-                element.getNodeName());
+        JAXBElement<ActivityTrackpointExtensionT> root = unmarshaller.unmarshal(source, ActivityTrackpointExtensionT.class);
+        return root.getValue();
     }
 }
