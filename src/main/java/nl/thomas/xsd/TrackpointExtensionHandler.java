8c7bea1f6dc3645754ca8e714a87c269e93ccdfa
Thomas Michels <thomas_michels@outlook.com>
2023-11-03 09:22:20 +0100

    Repareer testen en verbeter foutafhandeling TP extensions

--- src/main/java/nl/thomas/xsd/TrackpointExtensionHandler.java
+++ src/main/java/nl/thomas/xsd/TrackpointExtensionHandler.java
@@ -1,6 +1,7 @@
 package nl.thomas.xsd;
 
 import com.garmin.xmlschemas.activityextension.v2.ActivityTrackpointExtensionT;
+import com.garmin.xmlschemas.trainingcenterdatabase.v2.ExtensionsT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrackpointT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
 import jakarta.xml.bind.JAXBContext;
@@ -20,6 +21,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -31,19 +33,21 @@ public class TrackpointExtensionHandler {
     }
 
     private void replaceIncorrectlyParsedExtension(TrackpointT trackpointT) {
-        trackpointT
-                .getExtensions()
-                .getAny()
-                .set(0, parseExtension(trackpointT));
+        ExtensionsT extensions = trackpointT.getExtensions();
+        if (extensions == null || extensions.getAny() == null || extensions.getAny().isEmpty()) {
+            log.warn("No extensions found in trackpoint {}", trackpointT.getTime());
+            return;
+        }
+        parseExtension(trackpointT).ifPresent(tp -> extensions.getAny().set(0, tp));
     }
 
-    private ActivityTrackpointExtensionT parseExtension(TrackpointT trackpointT) {
+    private Optional<ActivityTrackpointExtensionT> parseExtension(TrackpointT trackpointT) {
         String rawXml = getRawXmlStringForExtension(trackpointT);
         try {
-            return convertXmlToObject(rawXml);
+            return Optional.of(convertXmlToObject(trackpointT, rawXml));
         } catch (JAXBException e) {
-            log.error("The TrackpointExtension is invalid and cannot be processed: \"{}\"", rawXml, e);
-            return null;
+            log.error("The TrackpointExtension for trackpoint {} is invalid and cannot be processed: \"{}\"", trackpointT.getTime(), rawXml);
+            return Optional.empty();
         }
     }
 
@@ -57,20 +61,22 @@ public class TrackpointExtensionHandler {
     }
 
     /**
-     * Rewrites for example the bolow XML
+     * Rewrites for example the below XML
      * <?xml version="1.0" encoding="UTF-16"?>
      * <x:TPX xmlns:x="http://www.garmin.com/xmlschemas/ActivityExtension/v2" xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-     *     <Speed xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2">0.0</Speed>
+     * <Speed xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2">0.0</Speed>
      * </x:TPX>
      *
+     * to
+     *
      * <x:TPX xmlns:x="http://www.garmin.com/xmlschemas/ActivityExtension/v2">
-     *     <Speed xmlns="http://www.garmin.com/xmlschemas/ActivityExtension/v2">0.0</Speed>
+     * <Speed xmlns="http://www.garmin.com/xmlschemas/ActivityExtension/v2">0.0</Speed>
      * </x:TPX>
      *
      * @param rawXml: Somehow the parser misreads the values of the extensions and interprets them as
-     *              <Speed xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2">0.0</Speed>
-     *              where it should be
-     *              <Speed xmlns="http://www.garmin.com/xmlschemas/ActivityExtension/v2">0.0</Speed>
+     *                <Speed xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2">0.0</Speed>
+     *                where it should be
+     *                <Speed xmlns="http://www.garmin.com/xmlschemas/ActivityExtension/v2">0.0</Speed>
      * @return corrected XML without unnecessary headers (this allows generic replacement of tag in case other extensions are provided)
      */
     private String correctRawXml(String rawXml) {
@@ -83,12 +89,28 @@ public class TrackpointExtensionHandler {
                         "http://www.garmin.com/xmlschemas/ActivityExtension/v2");
     }
 
-    private ActivityTrackpointExtensionT convertXmlToObject(String rawXml) throws JAXBException {
+    private ActivityTrackpointExtensionT convertXmlToObject(TrackpointT trackpointT, String rawXml) throws JAXBException {
         Unmarshaller unmarshaller = JAXBContext.newInstance(ActivityTrackpointExtensionT.class).createUnmarshaller();
         InputStream stream = new ByteArrayInputStream(rawXml.getBytes(StandardCharsets.UTF_8));
         Source source = new StreamSource(stream);
 
         JAXBElement<ActivityTrackpointExtensionT> root = unmarshaller.unmarshal(source, ActivityTrackpointExtensionT.class);
-        return root.getValue();
+        ActivityTrackpointExtensionT activityTrackpointExtensionT = root.getValue();
+        logIfAllValuesAreNull(trackpointT, rawXml, activityTrackpointExtensionT);
+        return activityTrackpointExtensionT;
+    }
+
+    private void logIfAllValuesAreNull(TrackpointT trackpointT, String rawXml, ActivityTrackpointExtensionT activityTrackpointExtensionT) {
+        if (allValuesOfExtensionAreNull(activityTrackpointExtensionT)) {
+            log.warn("ActivityTrackpointExtension for {} was correctly parsed, but all values are null. Raw XML: {}", trackpointT.getTime(), rawXml);
+        }
+    }
+
+    private boolean allValuesOfExtensionAreNull(ActivityTrackpointExtensionT activityTrackpointExtensionT) {
+        return activityTrackpointExtensionT.getExtensions() == null &&
+                activityTrackpointExtensionT.getCadenceSensor() == null &&
+                activityTrackpointExtensionT.getSpeed() == null &&
+                activityTrackpointExtensionT.getWatts() == null &&
+                activityTrackpointExtensionT.getRunCadence() == null;
     }
 }
--- src/test/java/nl/thomas/xsd/TrackpointExtensionHandlerTest.java
+++ src/test/java/nl/thomas/xsd/TrackpointExtensionHandlerTest.java
@@ -1,5 +1,6 @@
 package nl.thomas.xsd;
 
+import com.garmin.xmlschemas.activityextension.v2.ActivityTrackpointExtensionT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrackpointT;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
 import jakarta.xml.bind.JAXBException;
@@ -40,44 +41,42 @@ class TrackpointExtensionHandlerTest {
         assertThat(capturedOutput.getOut()).contains("No extensions found in trackpoint 2023-10-01T10:06:53Z");
         assertThat(trackpointT.getTime().toGregorianCalendar().toZonedDateTime()).isEqualTo(ZonedDateTime.parse("2023-10-01T10:06:53Z"));
         assertThat(trackpointT.getExtensions()).isNull();
-        assertThat(trackpointT.getSpeed()).isNull();
     }
 
     @Test
     void unexpectedExtension(CapturedOutput capturedOutput) throws JAXBException, IOException {
         TrackpointT trackpointT = getTrackpoints().get(TRACKPOINT_WITH_UNEXPECTED_EXTENSION);
 
-        assertThat(capturedOutput.getOut()).contains("Trackpoint 2023-10-01T10:06:54Z had another other extension \"x:OTHER\" than the expected \"x:TPX\" extension");
+        assertThat(capturedOutput.getOut()).contains("The TrackpointExtension for trackpoint 2023-10-01T10:06:54Z is invalid and cannot be processed: \"<?xml version=\"1.0\" encoding=\"UTF-16\"?><x:OTHER xmlns:x=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\" xmlns=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><Speed xmlns=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\">0.0</Speed></x:OTHER>\"");
         assertThat(trackpointT.getTime().toGregorianCalendar().toZonedDateTime()).isEqualTo(ZonedDateTime.parse("2023-10-01T10:06:54Z"));
-        assertThat(trackpointT.getExtensions().getAny().get(0).toString()).isEqualTo("[x:OTHER: null]");
-        assertThat(trackpointT.getSpeed()).isNull();
+        assertThat(trackpointT.getExtensions().getAny().get(0).toString()).hasToString("[x:OTHER: null]");
     }
 
     @Test
     void unexpectedNode(CapturedOutput capturedOutput) throws JAXBException, IOException {
         TrackpointT trackpointT = getTrackpoints().get(TRACKPOINT_WITH_UNEXPECTED_NODE);
 
-        assertThat(capturedOutput.getOut()).contains("Instead of speed node, the node NO_SPEED with value 3.17 was found for trackpoint 2023-10-01T10:06:55Z");
+        assertThat(capturedOutput.getOut()).contains("ActivityTrackpointExtension for 2023-10-01T10:06:55Z was correctly parsed, but all values are null. Raw XML: <x:TPX xmlns:x=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\"><NO_SPEED xmlns=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\">3.17</NO_SPEED></x:TPX>");
         assertThat(trackpointT.getTime().toGregorianCalendar().toZonedDateTime()).isEqualTo(ZonedDateTime.parse("2023-10-01T10:06:55Z"));
-        assertThat(trackpointT.getSpeed()).isNull();
+        assertThat(((ActivityTrackpointExtensionT) trackpointT.getExtensions().getAny().getFirst()).getSpeed()).isNull();
     }
 
     @Test
     void missingNode(CapturedOutput capturedOutput) throws JAXBException, IOException {
         TrackpointT trackpointT = getTrackpoints().get(TRACKPOINT_WITH_MISSING_NODE);
 
-        assertThat(capturedOutput.getOut()).contains("TPX node for Trackpoint 2023-10-01T10:06:56Z did not have the expected amount of 1 children, but 0 children");
+        assertThat(capturedOutput.getOut()).contains("The TrackpointExtension for trackpoint 2023-10-01T10:06:56Z is invalid and cannot be processed: \"<?xml version=\"1.0\" encoding=\"UTF-16\"?><x:TPX xmlns:x=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\" xmlns=\"http://www.garmin.com/xmlschemas/ActivityExtension/v2\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"/>");
         assertThat(trackpointT.getTime().toGregorianCalendar().toZonedDateTime()).isEqualTo(ZonedDateTime.parse("2023-10-01T10:06:56Z"));
-        assertThat(trackpointT.getSpeed()).isNull();
+        assertThat(trackpointT.getExtensions().getAny()).hasToString("[[x:TPX: null]]");
     }
 
     @Test
     void additionalNode(CapturedOutput capturedOutput) throws JAXBException, IOException {
         TrackpointT trackpointT = getTrackpoints().get(TRACKPOINT_WITH_ADDITIONAL_NODE);
 
-        assertThat(capturedOutput.getOut()).contains("TPX node for Trackpoint 2023-10-01T10:06:59Z did not have the expected amount of 1 children, but 2 children");
+        assertThat(capturedOutput.getOut()).doesNotContain("2023-10-01T10:06:59Z");
         assertThat(trackpointT.getTime().toGregorianCalendar().toZonedDateTime()).isEqualTo(ZonedDateTime.parse("2023-10-01T10:06:59Z"));
-        assertThat(trackpointT.getSpeed()).isEqualTo(3.42);
+        assertThat(((ActivityTrackpointExtensionT) trackpointT.getExtensions().getAny().getFirst()).getSpeed()).isEqualTo(3.42);
     }
 
     @Test
@@ -86,7 +85,7 @@ class TrackpointExtensionHandlerTest {
 
         assertThat(capturedOutput.getOut()).contains("No extensions found in trackpoint 2023-10-01T10:06:57Z");
         assertThat(trackpointT.getTime().toGregorianCalendar().toZonedDateTime()).isEqualTo(ZonedDateTime.parse("2023-10-01T10:06:57Z"));
-        assertThat(trackpointT.getSpeed()).isNull();
+        assertThat(trackpointT.getExtensions().getAny()).isEmpty();
     }
 
     private List<TrackpointT> getTrackpoints() throws IOException, JAXBException {
