8cc0ea4a97eadb4b1c94f35c4931739082be567d
Thomas Michels <thomas_michels@outlook.com>
2023-11-07 18:45:17 +0100

    Verwijder testcode

--- src/main/java/nl/thomas/xsd/XsdApplication.java
+++ src/main/java/nl/thomas/xsd/XsdApplication.java
@@ -1,35 +1,13 @@
 package nl.thomas.xsd;
 
-import jakarta.xml.bind.JAXBException;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class XsdApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(XsdApplication.class)
-                .run(args);
-    }
-
-    @Component
-    static class DevCommandLineRunner implements CommandLineRunner {
-
-        private final TcxController tcxController;
-
-        DevCommandLineRunner(TcxController tcxController) {
-            this.tcxController = tcxController;
-        }
-
-
-        @Override
-        public void run(String... args) throws JAXBException, IOException {
-            tcxController.file("src/test/java/testfiles/export_tomtom.tcx");
-        }
+        SpringApplication.run(XsdApplication.class, args);
     }
 
 }
