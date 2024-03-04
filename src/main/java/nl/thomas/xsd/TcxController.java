d406666fa577f8e60976ddaca4c7472d077280c8
Thomas Michels <thomas_michels@outlook.com>
2024-03-04 22:58:46 +0100

    Update controllers

--- src/main/java/nl/thomas/xsd/TcxController.java
+++ src/main/java/nl/thomas/xsd/TcxController.java
@@ -3,6 +3,8 @@ package nl.thomas.xsd;
 import com.garmin.xmlschemas.trainingcenterdatabase.v2.TrainingCenterDatabaseT;
 import jakarta.xml.bind.JAXBException;
 import lombok.extern.slf4j.Slf4j;
+import nl.thomas.xsd.model.Run;
+import nl.thomas.xsd.tcdbtomodel.TcdbConverter;
 import nl.thomas.xsd.tcxtotcdb.TcxParser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,22 +22,51 @@ import java.util.List;
 public class TcxController {
 
     private final TcxParser tcxParser;
+    private final TcdbConverter tcdbConverter;
 
-    public TcxController(TcxParser tcxParser) {
+    public TcxController(TcxParser tcxParser, TcdbConverter tcdbConverter) {
         this.tcxParser = tcxParser;
+        this.tcdbConverter = tcdbConverter;
     }
 
-    @PostMapping("file")
-    public TrainingCenterDatabaseT file(@RequestBody String location) throws IOException, JAXBException {
+    @PostMapping("/raw/file")
+    public TrainingCenterDatabaseT fileForTcdb(@RequestBody String location) throws IOException, JAXBException {
         log.info("GET request to read {}", location);
+        String fileContent = readFileContent(location);
+        return parseContentToTcdb(fileContent);
+    }
+
+    @PostMapping("/raw/file-content")
+    public TrainingCenterDatabaseT getFromFileContentForTcdb(@RequestBody String fileContent) throws JAXBException {
+        log.info("GET request to read text with {} characters", fileContent.length());
+        return parseContentToTcdb(fileContent);
+    }
+
+    @PostMapping("/model/file")
+    public List<Run> fileForModel(@RequestBody String location) throws IOException, JAXBException {
+        log.info("GET request to read {}", location);
+        String fileContent = readFileContent(location);
+        TrainingCenterDatabaseT tcdb = parseContentToTcdb(fileContent);
+        return convertTcdb(tcdb);
+    }
+
+    @PostMapping("/model/file-content")
+    public List<Run> getFromFileContentForModel(@RequestBody String fileContent) throws JAXBException {
+        log.info("GET request to read text with {} characters", fileContent.length());
+        TrainingCenterDatabaseT tcdb = parseContentToTcdb(fileContent);
+        return convertTcdb(tcdb);
+    }
+
+    private String readFileContent(String location) throws IOException {
         List<String> lines = Files.readAllLines(Path.of(location));
-        String fileContent = String.join("", lines);
+        return String.join("", lines);
+    }
+
+    private TrainingCenterDatabaseT parseContentToTcdb(String fileContent) throws JAXBException {
         return tcxParser.parse(fileContent);
     }
 
-    @PostMapping("file-content")
-    public TrainingCenterDatabaseT getFromFileContent(@RequestBody String fileContent) throws JAXBException {
-        log.info("GET request to read text with {} characters", fileContent.length());
-        return tcxParser.parse(fileContent);
+    private List<Run> convertTcdb(TrainingCenterDatabaseT tcdb) {
+        return tcdbConverter.convert(tcdb);
     }
 }
--- src/test/java/nl/thomas/xsd/TcxControllerTest.java
+++ src/test/java/nl/thomas/xsd/TcxControllerTest.java
@@ -1,6 +1,7 @@
 package nl.thomas.xsd;
 
 import jakarta.xml.bind.JAXBException;
+import nl.thomas.xsd.tcdbtomodel.TcdbConverter;
 import nl.thomas.xsd.tcxtotcdb.TcxParser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +14,9 @@ import org.springframework.boot.test.system.OutputCaptureExtension;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith({MockitoExtension.class, OutputCaptureExtension.class})
 class TcxControllerTest {
@@ -22,21 +25,43 @@ class TcxControllerTest {
     private TcxController tcxController;
     @Mock
     private TcxParser tcxParser;
+    @Mock
+    private TcdbConverter tcdbConverter;
 
     @Test
-    void path_readPath_contentToParser(CapturedOutput capturedOutput) throws JAXBException, IOException {
-        tcxController.file("src/test/java/testfiles/invalidxml.txt");
+    void path_readPathForTcdb_contentToParser(CapturedOutput capturedOutput) throws JAXBException, IOException {
+        tcxController.fileForTcdb("src/test/java/testfiles/invalidxml.txt");
 
         assertThat(capturedOutput.getOut()).contains("GET request to read src/test/java/testfiles/invalidxml.txt");
         verify(tcxParser).parse("Dit bestandis geenTCX bestand");
+        verifyNoInteractions(tcdbConverter);
     }
 
     @Test
-    void content_readContent_contentToParser(CapturedOutput capturedOutput) throws JAXBException {
-        tcxController.getFromFileContent("dit is TCX tekst");
+    void content_readContentForTcdb_contentToParser(CapturedOutput capturedOutput) throws JAXBException {
+        tcxController.getFromFileContentForTcdb("dit is TCX tekst");
 
         assertThat(capturedOutput.getOut()).contains("GET request to read text with 16 characters");
         verify(tcxParser).parse("dit is TCX tekst");
+        verifyNoInteractions(tcdbConverter);
+    }
+
+    @Test
+    void path_readPathForModel_contentToParser(CapturedOutput capturedOutput) throws JAXBException, IOException {
+        tcxController.fileForModel("src/test/java/testfiles/invalidxml.txt");
+
+        assertThat(capturedOutput.getOut()).contains("GET request to read src/test/java/testfiles/invalidxml.txt");
+        verify(tcxParser).parse("Dit bestandis geenTCX bestand");
+        verify(tcdbConverter).convert(any());
+    }
+
+    @Test
+    void content_readContentForModel_contentToParser(CapturedOutput capturedOutput) throws JAXBException {
+        tcxController.getFromFileContentForModel("dit is TCX tekst");
+
+        assertThat(capturedOutput.getOut()).contains("GET request to read text with 16 characters");
+        verify(tcxParser).parse("dit is TCX tekst");
+        verify(tcdbConverter).convert(any());
     }
 
 }
