1e497fba40f83dc6e4cbef37aecd6b84a95ce96d
Thomas Michels <thomas_michels@outlook.com>
2024-03-22 06:48:50 +0100

    Improve OpenApi documentation

--- src/main/java/nl/thomas/xsd/config/OpenAPIConfig.java
+++ src/main/java/nl/thomas/xsd/config/OpenAPIConfig.java
@@ -1,8 +1,6 @@
 package nl.thomas.xsd.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -13,18 +11,18 @@ public class OpenAPIConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components().addHeaders("hoi", new Header().required(false)))
                 .info(new Info()
                         .title("TCX to Json")
-                        .version("0.1")
+                        .version("1.0")
                         .description(
-                                "This application reads .tcx files that comply with the official xsd specifications found " +
+                                "This application reads .tcx files that comply with the official xml specifications found " +
                                         "here https://www8.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd, and " +
-                                        "returns them as JSON object." +
+                                        "and here https://www8.garmin.com/xmlschemas/ActivityExtensionv2.xsd and returns " +
+                                        "them as JSON." +
                                         "<br>" +
                                         "<br>" +
-                                        "Input is either the path of a tcx file, or the full content of a tcx file. " +
-                                        "Output is either the exact translation of tcx to json, or a simplified and opionated model. " +
+                                        "Input is either the path to a tcx file in the file system, or the full content of a tcx file. " +
+                                        "Output is either the exact translation of tcx to json, or a simplified (opinionated) model. " +
                                         "<br>Tested on exports of Strava, Garmin, TomTom, SmashRun and ttbin2tcx (https://github.com/alexspurven/ttbin2tcx), " +
                                         "but should work on all TCX applications that comply with the xsd.")
                         .contact(new Contact()
