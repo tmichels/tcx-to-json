f061d2ddc5a489f9d94e7f86f9fa081ab3de5f7c
Thomas Michels <thomas_michels@outlook.com>
2024-03-31 13:57:02 +0200

    Rename application booter

--- src/main/java/nl/tmichels/tcxtojson/TcxToJsonApplication.java
+++ src/main/java/nl/tmichels/tcxtojson/TcxToJsonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XsdApplication {
+public class TcxToJsonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(XsdApplication.class, args);
+        SpringApplication.run(TcxToJsonApplication.class, args);
     }
 
 }
